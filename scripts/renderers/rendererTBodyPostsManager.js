cfcbaaa1d757b6571de1d5b9fe9910c335b95758
joao_junio <joaojunio818@gmail.com>
2026-02-12 22:37:09 -0300

    Carregando template e gerando posts no gerenciador

--- scripts/renderers/rendererTBodyPostsManager.js
+++ scripts/renderers/rendererTBodyPostsManager.js
@@ -1,6 +1,14 @@
 export function rendererTBodyPostsManager(posts, tbody) {
 	const template = document.getElementById("template-posts-manager");
-	const content = template.content.cloneNode(true);
-	tbody.appendChild(content);
+	const contentTbody = template.content.cloneNode(true);
 	console.log(posts);
+
+	// TEREI QUE FAZER DA MESMA FORMA QUE O: rendererNextPosts.js -> aqui vai ter que ser html direto na string.
+
+	posts.forEach(post => {
+		const title = contentTbody.querySelector("#title");
+		title.textContent = post.title;
+		
+
+	});
 }
