1f048fe4f261c24ce203e7a624da21298db9ab3f
joao_junio <joaojunio818@gmail.com>
2026-02-14 17:30:14 -0300

    Iniciando implementação de cadastro de usuario com PHP

--- scripts/services/postService.js
+++ scripts/services/postService.js
@@ -9,7 +9,7 @@ const GET_IMAGE_FROM_POST_URL = `${BASE_URL}/api/posts/v1/getImageFromPost/{file
 const UPDATE_POST_URL = `${BASE_URL}/api/posts/v1`;
 const DELETE_POST_URL = `${BASE_URL}/api/posts/v1/{postId}`;
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE3NzEwMzMyNjcsImV4cCI6MTc3MTAzNjg2Nywic3ViIjoiam90YWpvdGEiLCJyb2xlcyI6W119.Tq19UaaOzZNXb9qoASl6ky2fBDONBAcofeStE9NO1uA";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE3NzEwOTQ0MDAsImV4cCI6MTc3MTA5ODAwMCwic3ViIjoiam90YWpvdGEiLCJyb2xlcyI6W119.S33oy_2ZPi0a369r1mmxlEHOCCegdW6OfzDXOboHhZk";
 
 async function findAll(contentType) {
 	try {
